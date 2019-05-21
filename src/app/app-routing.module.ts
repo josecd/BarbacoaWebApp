faef71d64179a2050a54e71bbd2135848eec242a
josecd <jose-cordero.dz@hotmail.com>
2019-05-21 12:22:22 -0500

    Plantilla casi modificada.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { AdministradorComponent } from './pages/administrador/administrador.component';
+import { SucursalesComponent } from './pages/sucursales/sucursales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component : HomeComponent},
+  {path: 'home', component : HomeComponent},
+  {path: 'sobre', component : SobreComponent},
+  {path: 'contacto', component : ContactoComponent},
+  {path: 'menu', component : MenuComponent},
+  {path: 'admin', component : AdministradorComponent},
+  {path: 'sucursales', component : SucursalesComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, enableProdMode } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { AdministradorComponent } from './pages/administrador/administrador.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { SucursalesComponent } from './pages/sucursales/sucursales.component';
+enableProdMode();
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SobreComponent,
+    AdministradorComponent,
+    ContactoComponent,
+    FooterComponent,
+    HeaderComponent,
+    MenuComponent,
+    SucursalesComponent
   ],
   imports: [
     BrowserModule,
