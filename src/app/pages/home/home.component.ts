4463c37b87469d8d86fe65616f585859f33251b8
GerardoJCT <darck9900@gmail.com>
2019-06-06 15:47:39 -0600

    Scroll to top
    siguele solo falta sobre poner el boton sobre las imaenes  y algo de diseño

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ServicioCargaService } from 'src/app/servicios/servicio-carga.service';
 import { AppComponent } from 'src/app/app.component';
 @Component({
@@ -9,8 +9,23 @@ import { AppComponent } from 'src/app/app.component';
 export class HomeComponent implements OnInit {
 
 // tslint:disable-next-line: variable-name
+showScroll: boolean;
+showScrollHeight = 200;
+hideScrollHeight = 100;
   constructor(public _servicioProducto: ServicioCargaService) {
   }
+  @HostListener('window:scroll', [])
+    onWindowScroll() 
+    {
+      if (( window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) > this.showScrollHeight) 
+      {
+        this.showScroll = true;
+      } 
+      else if ( this.showScroll && (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) < this.hideScrollHeight) 
+      { 
+        this.showScroll = false; 
+      }
+    }
 
   ngOnInit() {
     // this.servicioProducto.getMenu().subscribe(products => {
@@ -19,4 +34,15 @@ export class HomeComponent implements OnInit {
     // });
 
   }
+  scrollToTop() 
+    { 
+      (function smoothscroll() 
+      { var currentScroll = document.documentElement.scrollTop || document.body.scrollTop; 
+        if (currentScroll > 0) 
+        {
+          window.requestAnimationFrame(smoothscroll);
+          window.scrollTo(0, currentScroll - (currentScroll / 5));
+        }
+      })();
+    }
 }
